87f5fdf4ab0af16391eeeac82261d867164b85ca
Polo123456789 <pabsan02@gmail.com>
2023-11-24 12:39:28 -0600

    cambios xd

--- psg-open-file.sh
+++ psg-open-file.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
-xdg-open "$(zenity --file-selection)"
+
+# xdg-open "$(zenity --file-selection)"
+
+SELECTION=""
+CURRENT_DIRECTORY="$HOME"
+
+while [ ! -f "$SELECTION" ]; do
+    cd "$CURRENT_DIRECTORY"
+    SELECTION=$(ls -1a | dmenu -i -p "File:" -l 15)
+    if [ -d "$SELECTION" ]; then
+        CURRENT_DIRECTORY="$SELECTION"
+    elif [ -f "$SELECTION" ]; then
+        xdg-open "$SELECTION"
+    else
+        exit 0
+    fi
+done
