f8012f489c4f58f75c8b21a0990c4a628243838b
Polo123456789 <pabsan02@gmail.com>
2023-12-21 10:08:06 -0600

    Total Revamp of rclonw-drive-sync, and other goodies

--- psg-open-file.sh
+++ psg-open-file.sh
@@ -5,13 +5,24 @@
 SELECTION=""
 CURRENT_DIRECTORY="$HOME"
 
+get_options() {
+    if [ "$CURRENT_DIRECTORY" = "$HOME" ]; then
+        echo "@clipboard"
+    fi
+    ls -1a
+}
+
 while [ ! -f "$SELECTION" ]; do
     cd "$CURRENT_DIRECTORY"
-    SELECTION=$(ls -1a | dmenu -i -p "File:" -l 15)
-    if [ -d "$SELECTION" ]; then
+    SELECTION=$(get_options | dmenu -i -p "File:" -l 15)
+    if [ "$SELECTION" = "@clip" ] || [ "$SELECTION" = "@clipboard" ]; then
+        xdg-open "$(xclip -o -selection clipboard)"
+        exit 0
+    elif [ -d "$SELECTION" ]; then
         CURRENT_DIRECTORY="$SELECTION"
     elif [ -f "$SELECTION" ]; then
         xdg-open "$SELECTION"
+        exit 0
     else
         exit 0
     fi
